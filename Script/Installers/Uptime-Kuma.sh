65ceae4bbe9404ac4d861fd683503014959992fc
rune <christian.rune1998@gmail.com>
2023-12-14 17:12:57 +0100

    Testing new compose methode

--- Script/Installers/Uptime-Kuma.sh
+++ Script/Installers/Uptime-Kuma.sh
@@ -23,22 +23,28 @@ COMPOSE_SUBFOLDER="./RRHQD-Dockers/compose-files"
 COMPOSE_FILE="$COMPOSE_SUBFOLDER/docker-compose-$CONTAINER_NAME.yml"
 
 # Create the subfolder if it does not exist
-mkdir -p $COMPOSE_SUBFOLDER
+mkdir -p "$COMPOSE_SUBFOLDER"
 
 # Create a Docker compose file with the user input inside the subfolder
-cat > $COMPOSE_FILE <<EOF
-version: '3'
-services:
-  $CONTAINER_NAME:
-    image: $IMAGE
-    ports:
-      - "$PORT:3001"
-    volumes:
-      - $DATA_PATH:/app/data
-EOF
+{
+  echo "version: '3'"
+  echo "services:"
+  echo "  $CONTAINER_NAME:"
+  echo "    image: $IMAGE"
+  echo "    ports:"
+  echo "      - \"$PORT:3001\""
+  echo "    volumes:"
+  echo "      - \"$DATA_PATH:/app/data\""
+} > "$COMPOSE_FILE"
 
 # Inform the user where the Docker compose file has been created
 echo "Docker compose file created at: $COMPOSE_FILE"
 
+# Check if Docker is running
+if ! docker info >/dev/null 2>&1; then
+    echo "Docker does not seem to be running, start it first and then re-run this script."
+    exit 1
+fi
+
 # Start the Docker container using docker-compose
-docker compose up -d
+docker compose -f "$COMPOSE_FILE" up -d
