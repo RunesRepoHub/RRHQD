1f2e9d8b03f9f897c6f51367b7d00a7448ca51dd
rune <christian.rune1998@gmail.com>
2023-12-14 15:41:34 +0100

    Added code in case of lacking user input

--- Script/Installers/Uptime-Kuma.sh
+++ Script/Installers/Uptime-Kuma.sh
@@ -5,11 +5,18 @@ source ~/RRHQD/Core/Core.sh
 
 echo -e "${Green}Setup a Docker container for Uptime-Kuma${NC}"
 
-# Prompt user for input
+# Prompt user for input with defaults
 read -p "Enter the Docker image for Uptime-Kuma (e.g., louislam/uptime-kuma:1): " IMAGE
+IMAGE=${IMAGE:-"louislam/uptime-kuma:1"}
+
 read -p "Enter the name for the Uptime-Kuma container: " CONTAINER_NAME
+CONTAINER_NAME=${CONTAINER_NAME:-"uptime-kuma-container"}
+
 read -p "Enter the port to expose Uptime-Kuma on (e.g., 3001): " PORT
+PORT=${PORT:-3001}
+
 read -p "Enter the path for Uptime-Kuma data (e.g., /kuma-data/): " DATA_PATH
+DATA_PATH=${DATA_PATH:-./kuma-data}
 
 # Create a Docker compose file with the user input
 cat > docker-compose.yml <<EOF
