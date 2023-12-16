49110c3e5c2f337d0c73eea8344fa025b2dd537d
rune <christian.rune1998@gmail.com>
2023-12-16 12:21:49 +0100

    Testing fix for functions issues

--- Setup.sh
+++ Setup.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-# Check if curl is installed and install it if not
-check_and_install "curl" "https://my-curl-install-script.com"
-
-# Refactored code block
+# Function to check and install missing packages
 check_and_install() {
-    local pkg=$1
-    local install_script=$2
-    if ! command -v "$pkg" &> /dev/null; then
-        echo "$pkg is not installed. Attempting to install $pkg..."
-        if [ -n "$install_script" ]; then
-            curl -fsSL "$install_script" -o get-$pkg.sh
-            sh get-$pkg.sh
-            if ! command -v "$pkg" &> /dev/null; then
-                echo "Failed to install $pkg. Aborting script."
-                exit 1
-            fi
-            echo "$pkg has been successfully installed."
-        else
-            echo "$pkg is not installed. Aborting script."
+    local package=$1
+    if ! command -v "$package" &> /dev/null; then
+        echo "$package is not installed. Attempting to install $package..."
+        sudo apt-get update && sudo apt-get install -y "$package"
+        if ! command -v "$package" &> /dev/null; then
+            echo "Failed to install $package. Aborting script."
             exit 1
         fi
+        echo "$package has been successfully installed."
+    else
+        echo "$package is already installed."
     fi
 }
 
-# Check if git is installed
-check_and_install "git"
+# Check and install git if necessary
+check_and_install git
 
-# Check if sudo is installed
-check_and_install "sudo"
+# Check and install sudo if necessary
+check_and_install sudo
+
+# Check and install curl if necessary
+check_and_install curl
 
 # Check if Docker is installed and install it if not
-check_and_install "docker" "https://get.docker.com"
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Attempting to install Docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    if ! command -v docker &> /dev/null; then
+        echo "Failed to install Docker. Aborting script."
+        exit 1
+    fi
+    echo "Docker has been successfully installed."
+fi
 
 
 # Define the GitHub repository URL
