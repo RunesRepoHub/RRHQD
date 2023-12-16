ca96f2c1685576d9178570a45e7d534a249d3d5b
rune <christian.rune1998@gmail.com>
2023-12-16 12:16:27 +0100

    Testing new functions

--- Setup.sh
+++ Setup.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-# Check if Docker is installed
+# Check if Docker is installed and install it if not
 if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Aborting script."
-    exit 1
+    echo "Docker is not installed. Attempting to install Docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    if ! command -v docker &> /dev/null; then
+        echo "Failed to install Docker. Aborting script."
+        exit 1
+    fi
+    echo "Docker has been successfully installed."
 fi
+
+check_and_install() {
+    local pkg=$1
+    local install_script=$2
+    if ! command -v "$pkg" &> /dev/null; then
+        echo "$pkg is not installed. Attempting to install $pkg..."
+        if [ -n "$install_script" ]; then
+            curl -fsSL "$install_script" -o get-$pkg.sh
+            sh get-$pkg.sh
+            if ! command -v "$pkg" &> /dev/null; then
+                echo "Failed to install $pkg. Aborting script."
+                exit 1
+            fi
+            echo "$pkg has been successfully installed."
+        else
+            echo "$pkg is not installed. Aborting script."
+            exit 1
+        fi
+    fi
+}
 
 # Check if git is installed
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Aborting script."
-    exit 1
-fi
+check_and_install "git"
 
 # Check if sudo is installed
-if ! command -v sudo &> /dev/null; then
-    echo "Sudo is not installed. Aborting script."
-    exit 1
-fi
+check_and_install "sudo"
 
 # Check if curl is installed
-if ! command -v curl &> /dev/null; then
-    echo "Curl is not installed. Aborting script."
-    exit 1
-fi
+check_and_install "curl"
+
+# Check if Docker is installed and install it if not
+check_and_install "docker" "https://get.docker.com"
 
 
 # Define the GitHub repository URL
