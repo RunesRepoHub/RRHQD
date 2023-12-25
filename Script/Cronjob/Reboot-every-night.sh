343775bea098dd91d9c3143c3f34dfd621d11c4c
rune <christian.rune1998@gmail.com>
2023-12-25 20:05:25 +0100

    Updated cronjob

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -4,13 +4,13 @@ source ~/RRHQD/Core/Core.sh
 
 echo -e "${Green}Adding reboot cron job...${NC}"
 
-# Check if the reboot cron job already exists
-if crontab -l | grep -q "45 4 \* \* \* /sbin/reboot"; then
-    echo -e "${Red}Reboot cron job already exists. Aborting script.${NC}"
+# Check if the reboot cron job already exists in /etc/crontab
+if grep -q "45 4 \* \* \* root /sbin/reboot" /etc/crontab; then
+    echo -e "${Red}Reboot cron job already exists in /etc/crontab. Aborting script.${NC}"
     exit 1
 else
-    # Add the reboot cron job
-    (crontab -l 2>/dev/null; echo "45 4 * * * /sbin/reboot") | crontab -
-    echo -e "${Green}Reboot cron job added successfully.${NC}"
+    # Add the reboot cron job to /etc/crontab
+    echo "45 4 * * * root /sbin/reboot" >> /etc/crontab
+    echo -e "${Green}Reboot cron job added to /etc/crontab successfully.${NC}"
 fi
 
