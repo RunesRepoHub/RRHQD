715973fc601d0fc5cf10b5e4d35f3cbc56b0472f
rune <christian.rune1998@gmail.com>
2024-01-02 21:05:41 +0100

    Testing

--- Script/Docker-CnC/Docker-Start.sh
+++ Script/Docker-CnC/Docker-Start.sh
@@ -5,18 +5,18 @@ clear
 source ~/RRHQD/Core/Core.sh
 
 # Define the input file for dialog selections
-INPUT=/tmp/containers.sh.$$
+INPUT="/tmp/containers.sh.$$"
 
 # Ensure the temp file is removed upon script termination
-trap "rm -f $INPUT" 0 1 2 5 15
+trap 'rm -f "$INPUT"' EXIT INT TERM HUP
 
 # Get the list of stopped containers
-stopped_containers=$(docker ps -a --filter "status=exited" --format '{{.Names}}')
+mapfile -t stopped_containers < <(docker ps -a --filter "status=exited" --format '{{.Names}}')
 
 # Create an array for dialog options
 options=()
-for container in $stopped_containers; do
-    options+=("$container" "")
+for container in "${stopped_containers[@]}"; do
+    options+=("$container" "" OFF)
 done
 
 # Use dialog to create a menu with stopped containers
@@ -24,13 +24,19 @@ dialog --clear \
        --backtitle "RRHQD Docker Control" \
        --title "Start Docker Containers" \
        --checklist "Select containers to start:" 15 60 6 \
-       "${options[@]}" 2>"${INPUT}"
+       "${options[@]}" 2>"$INPUT"
 
-# Start selected containers
-while IFS= read -r container; do
-    echo "Starting $container..."
-    docker start "$container"
-done < "${INPUT}"
+# Check if the dialog was cancelled
+if [ $? -eq 0 ]; then
+    # Start selected containers
+    while IFS= read -r container; do
+        echo "Starting $container..."
+        docker start "$container"
+    done < "$INPUT"
+else
+    echo "Container start cancelled."
+fi
 
 # Remove the temp file
-rm -f "${INPUT}"
+rm -f "$INPUT"
+
