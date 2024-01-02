64b8a7798bba67d78004ebd593d16a2c6b1005ad
rune <christian.rune1998@gmail.com>
2024-01-02 20:29:13 +0100

    Minor design test

--- Script/Menu/Main-Menu.sh
+++ Script/Menu/Main-Menu.sh
@@ -41,7 +41,7 @@ function show_dialog_menu() {
             echo -e "${Green}You can now run the script fully updated${NC}"
             ;;
         *)
-            dialog --title "Exiting" --msgbox "Thank you for using RRHQD. Support me via github https://github.com/RunesRepoHub/" 6 44
+            dialog --title "Exiting" --msgbox "Thank you for using RRHQD. Support me via github https://github.com/RunesRepoHub/" 6 64
             exit 0
             ;;
     esac
