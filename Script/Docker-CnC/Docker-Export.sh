e82939272da1ac5583d6ea6e048a24dae85c22b7
rune <christian.rune1998@gmail.com>
2024-01-07 22:25:23 +0100

    More debug

--- Script/Docker-CnC/Docker-Export.sh
+++ Script/Docker-CnC/Docker-Export.sh
@@ -10,9 +10,25 @@ IMAGE_NAME=""
 PORTS=""
 VOLUMES=""
 
+# Function to increment log file name
+increment_log_file_name() {
+  local log_file_base_name="docker_export_run_"
+  local log_file_extension=".log"
+  local log_file_counter=1
+
+  while [[ -f "$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}" ]]; do
+    ((log_file_counter++))
+  done
+
+  LOG_FILE="$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}"
+  echo "Log file will be saved as $LOG_FILE"
+}
+
 # Create log directory if it doesn't exist
 mkdir -p "$LOG_DIR"
-LOG_FILE="$LOG_DIR/docker_export.log"
+
+# Increment log file name for this run
+increment_log_file_name
 
 # Redirect all output to the log file
 exec > >(tee -a "$LOG_FILE") 2>&1
