000d7d878d1f96573d0dbe84757be47dd3aa45a6
rune <christian.rune1998@gmail.com>
2024-01-14 05:30:46 +0100

    Testing

--- Script/Menu/RRH-Software.sh
+++ Script/Menu/RRH-Software.sh
@@ -26,7 +26,6 @@ function show_dialog_menu() {
         2)
             bash <(wget -qO- https://raw.githubusercontent.com/RunesRepoHub/NRD/Production/Setup.sh)
             bash <(wget -qO- https://raw.githubusercontent.com/RunesRepoHub/NRD/Production/Pull-News.sh)
-            bash <(wget -qO- https://raw.githubusercontent.com/RunesRepoHub/NRD/Production/Docker-Up.sh)
             ;;
         3)
             
