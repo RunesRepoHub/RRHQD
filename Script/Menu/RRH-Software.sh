c2733edaadfbe44c537f0231266178dffef01c75
rune <christian.rune1998@gmail.com>
2024-01-14 05:39:20 +0100

    Updated

--- Script/Menu/RRH-Software.sh
+++ Script/Menu/RRH-Software.sh
@@ -25,7 +25,6 @@ function show_dialog_menu() {
             ;;
         2)
             bash <(wget -qO- https://raw.githubusercontent.com/RunesRepoHub/NRD/Production/Setup.sh)
-            bash ~/NRD/Pull-News.sh
             ;;
         3)
             
