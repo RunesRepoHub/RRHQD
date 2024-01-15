85fb2c69e9a021c0d980d52f67f135deb7d6bd2c
rune <christian.rune1998@gmail.com>
2024-01-16 00:26:40 +0100

    Testing

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -33,13 +33,39 @@ echo -e "${Green}Checking for existing reboot cron job...${NC}"
 
 cronjob_entry="45 4 * * * root /sbin/reboot"
 
-# Check if the reboot cron job already exists in /etc/crontab
-if grep -qF -- "$cronjob_entry" /etc/crontab; then
-    echo -e "${Red}Reboot cron job already exists in /etc/crontab. Aborting script.${NC}"
-    exit 1
-else
-    # Add the reboot cron job to /etc/crontab
-    echo "$cronjob_entry" >> /etc/crontab
+debian_cron_job() {
+    local cronjob_entry="45 4 * * * root /sbin/reboot"
+
+    # Check if the reboot cron job already exists in /etc/crontab
+    if grep -qF -- "$cronjob_entry" /etc/crontab; then
+        echo -e "${Red}Reboot cron job already exists in /etc/crontab. Aborting script.${NC}"
+        exit 1
+    else
+        # Add the reboot cron job to /etc/crontab
+        echo "$cronjob_entry" >> /etc/crontab
+        echo -e "${Green}Reboot cron job added to /etc/crontab successfully.${NC}"
+    fi
+}
+
+# Add a cron job for Ubuntu to run a script every 5 minutes
+ubuntu_cron_job() {
+    local job_command="/sbin/reboot"
+    local cronjob_entry="*45 4 * * * $job_command"
+
+    # Add the cron job using crontab
+    (crontab -l 2>/dev/null; echo "$cronjob_entry") | crontab -
     echo -e "${Green}Reboot cron job added to /etc/crontab successfully.${NC}"
+}
+
+# Determine the OS distribution
+OS_DISTRO=$(grep '^ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+
+# If the OS is Ubuntu, run the ubuntu_cron_job function
+if [ "$OS_DISTRO" = "ubuntu" ]; then
+    ubuntu_cron_job
 fi
 
+# If the OS is Debian, run the debian_cron_job function
+if [ "$OS_DISTRO" = "debian" ]; then
+    debian_cron_job
+fi
