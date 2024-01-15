c6070faecb6b6bcac7b1c10588dae70985c9e6e1
rune <christian.rune1998@gmail.com>
2024-01-15 23:30:48 +0100

    testing

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
+# Configuration for update script
+LOG_DIR="$HOME/RRHQD/logs"
+LOG_FILE="$LOG_DIR/update.log"  # Log file location
+
+# Function to increment log file name
+increment_log_file_name() {
+  local log_file_base_name="update_run_"
+  local log_file_extension=".log"
+  local log_file_counter=1
+
+  while [[ -f "$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}" ]]; do
+    ((log_file_counter++))
+  done
+
+  LOG_FILE="$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}"
+  echo "Log file for update will be saved as $LOG_FILE"
+}
+
+# Create log directory if it doesn't exist
+mkdir -p "$LOG_DIR"
+
+# Increment log file name for this run
+increment_log_file_name
+
+# Redirect all output to the log file
+exec > >(tee -a "$LOG_FILE") 2>&1
+
 source ~/RRHQD/Core/Core.sh
 
 echo -e "${Green}Checking for existing reboot cron job...${NC}"
 
-# Determine the current user for the cron job
-current_user=$(whoami)
+cronjob_entry="45 4 * * * root /sbin/reboot"
 
-# Set the cron job entry for reboot
-cronjob_entry="45 4 * * * $current_user /sbin/reboot"
+# Check if the reboot cron job already exists in /etc/crontab
+if grep -qF -- "$cronjob_entry" /etc/crontab; then
+    echo -e "${Red}Reboot cron job already exists in /etc/crontab. Aborting script.${NC}"
+    exit 1
+else
+    # Add the reboot cron job to /etc/crontab
+    echo "$cronjob_entry" >> /etc/crontab
+    echo -e "${Green}Reboot cron job added to /etc/crontab successfully.${NC}"
+fi
 
-# Function to add cron job entry to user's own crontab
-add_user_cron_job() {
-    # Check if the reboot cron job already exists in the user's crontab
-    if crontab -l | grep -qF -- "$cronjob_entry"; then
-        echo -e "${Red}Reboot cron job already exists. Aborting script.${NC}"
-        exit 1
-    else
-        # Add the reboot cron job to the user's crontab
-        (crontab -l 2>/dev/null; echo "$cronjob_entry") | crontab -
-        echo -e "${Green}Reboot cron job added successfully.${NC}"
-    fi
-}
 
-# Call the function to add the cron job
-add_user_cron_job
-
+# Check if the cron job already exists in the user's crontab
+if crontab -l | grep -qF -- "$cronjob_entry"; then
+    echo -e "${Red}Cron job already exists. No changes made.${NC}"
+else
+    # Write out current crontab and add the new cron job
+    (crontab -l; echo "$cronjob_entry") | crontab -
+    echo -e "${Green}Cron job added successfully.${NC}"
+fi
