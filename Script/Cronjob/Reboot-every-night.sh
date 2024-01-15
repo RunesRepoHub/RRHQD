bb4fcfeeae1eee3091ab74f0cdee4be9c19ff65e
rune <christian.rune1998@gmail.com>
2024-01-15 23:23:59 +0100

    Testing

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# Configuration for update script
-LOG_DIR="$HOME/RRHQD/logs"
-LOG_FILE="$LOG_DIR/update.log"  # Log file location
-
-# Function to increment log file name
-increment_log_file_name() {
-  local log_file_base_name="update_run_"
-  local log_file_extension=".log"
-  local log_file_counter=1
-
-  while [[ -f "$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}" ]]; do
-    ((log_file_counter++))
-  done
-
-  LOG_FILE="$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}"
-  echo "Log file for update will be saved as $LOG_FILE"
-}
-
-# Create log directory if it doesn't exist
-mkdir -p "$LOG_DIR"
-
-# Increment log file name for this run
-increment_log_file_name
-
-# Redirect all output to the log file
-exec > >(tee -a "$LOG_FILE") 2>&1
-
-
 source ~/RRHQD/Core/Core.sh
 
 echo -e "${Green}Checking for existing reboot cron job...${NC}"
