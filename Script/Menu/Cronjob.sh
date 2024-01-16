2337a42d6d4ae66aec069297f78ad4f891f29e7f
rune <christian.rune1998@gmail.com>
2024-01-16 02:40:09 +0100

    Hotfix

--- Script/Menu/Cronjob.sh
+++ Script/Menu/Cronjob.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+
 clear
 source ~/RRHQD/Core/Core.sh
 
 script_name=$(basename "$0" .sh)
 
-function show_options_menu() {
-    echo "Cronjob Menu - $script_name"
-    echo "Pick what cronjob you want to add:"
-    echo "1) Add a Nightly reboot at 4:45 am"
-    echo "2) Daily Midnight Update"
-    echo "3) Add a Reboot cron job for every Sunday at 00:00 am"
-    echo "4) Back To Main Menu"
-    echo -n "Please enter your choice: "
-    read menu_choice
+# Use dialog to create a more user-friendly menu
+function show_dialog_menu() {
+    dialog --clear \
+           --backtitle "RRHQD (RunesRepoHub Quick Deploy)" \
+           --title "Cronjob Menu - $script_name" \
+           --menu "Pick what cronjob you want to add:" 15 60 4 \
+           1 "Add a Nightly reboot at 4:45 am" \
+           2 "Daily Midnight Update" \
+           3 "Add a Reboot cron job for every Sunday at 00:00 am" \
+           4 "Back To Main Menu" 2>"${INPUT}"
 
+    menu_choice=$(<"${INPUT}")
     case $menu_choice in
         1)
             bash $ROOT_FOLDER/$SCRIPT_FOLDER/$CRONJOB_FOLDER/$REBOOT_EVERY_NIGHT
@@ -25,16 +28,20 @@ function show_options_menu() {
         3)
             bash $ROOT_FOLDER/$SCRIPT_FOLDER/$CRONJOB_FOLDER/$REBOOT_EVERY_SUNDAY
             ;;
-        4)  
-            exit 0
-            ;;
         *)
+            exit 0
             ;;
     esac
 }
 
+# Define the input file for dialog selections
+INPUT=/tmp/menu.sh.$$
+
+# Ensure the temp file is removed upon script termination
+trap "rm -f $INPUT" 0 1 2 5 15
+
 # Main loop
 while true; do
-    show_options_menu
+    show_dialog_menu
 done
 
