503d67a71bfe0c892a10008753c4459fc14ccf73
rune <christian.rune1998@gmail.com>
2024-01-16 01:43:09 +0100

    Rollback

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+# Configuration for update script
+LOG_DIR="$HOME/RRHQD/logs"
+LOG_FILE="$LOG_DIR/update.log"  # Log file location
+
+# Function to increment log file name
+increment_log_file_name() {
+  local log_file_base_name="update_run_"
+  local log_file_extension=".log"
+  local log_file_counter=1
+
+  while [[ -f "$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}" ]]; do
+    ((log_file_counter++))
+  done
+
+  LOG_FILE="$LOG_DIR/${log_file_base_name}${log_file_counter}${log_file_extension}"
+  echo "Log file for update will be saved as $LOG_FILE"
+}
+
+# Create log directory if it doesn't exist
+mkdir -p "$LOG_DIR"
+
+# Increment log file name for this run
+increment_log_file_name
+
+# Redirect all output to the log file
+exec > >(tee -a "$LOG_FILE") 2>&1
+
 source ~/RRHQD/Core/Core.sh
 
-#!/bin/bash
+echo -e "${Green}Checking for existing reboot cron job...${NC}"
 
-# Install AT command scheduler
-apt install at 
-sudo apt install at
+cronjob_entry="45 4 * * * root /sbin/reboot"
 
-# Define the reboot command without using systemd or cronjob/crontab
-reboot_command="/sbin/shutdown -r +5"
-
-# Determine the OS distribution
-os_distro=$(grep '^ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
-
-# Check if the current OS is one of the target distributions
-case "$os_distro" in
-  kali|ubuntu|debian|zorin|linuxmint)
-    # Schedule the reboot using at command
-    $reboot_command | at 04:45
-    ;;
-  *)
-    echo "This script does not support the current OS: $os_distro"
+# Check if the reboot cron job already exists in /etc/crontab
+if grep -qF -- "$cronjob_entry" /etc/crontab; then
+    echo -e "${Red}Reboot cron job already exists in /etc/crontab. Aborting script.${NC}"
     exit 1
-    ;;
-esac
+else
+    # Add the reboot cron job to /etc/crontab
+    echo "$cronjob_entry" >> /etc/crontab
+    echo -e "${Green}Reboot cron job added to /etc/crontab successfully.${NC}"
+fi
+
