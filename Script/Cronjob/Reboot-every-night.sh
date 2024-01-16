75746d99bff9c97462c2106b24990354d1905a27
rune <christian.rune1998@gmail.com>
2024-01-16 01:19:18 +0100

    Testing

--- Script/Cronjob/Reboot-every-night.sh
+++ Script/Cronjob/Reboot-every-night.sh
@@ -8,7 +8,8 @@ SERVICE_FILE="/etc/systemd/system/reboot-server.service"
 TIMER_FILE="/etc/systemd/system/reboot-server.timer"
 
 create_systemd_service() {
-    echo "[Unit]
+    cat << EOF | sudo tee $SERVICE_FILE > /dev/null
+[Unit]
 Description=Reboot the server at 4:45 AM
 
 [Service]
@@ -16,11 +17,13 @@ Type=oneshot
 ExecStart=/bin/systemctl reboot
 
 [Install]
-WantedBy=multi-user.target" | sudo tee $SERVICE_FILE > /dev/null
+WantedBy=multi-user.target
+EOF
 }
 
 create_systemd_timer() {
-    echo "[Unit]
+    cat << EOF | sudo tee $TIMER_FILE > /dev/null
+[Unit]
 Description=Trigger the server reboot service daily at 4:45 AM
 
 [Timer]
@@ -28,24 +31,20 @@ OnCalendar=*-*-* 04:45:00
 Persistent=true
 
 [Install]
-WantedBy=timers.target" | sudo tee $TIMER_FILE > /dev/null
+WantedBy=timers.target
+EOF
 }
 
 set_up_service_and_timer() {
-    # Check if the service and timer files already exist to avoid duplication
-    if [[ ! -f $SERVICE_FILE ]] && [[ ! -f $TIMER_FILE ]]; then
-        create_systemd_service
-        create_systemd_timer
+    create_systemd_service
+    create_systemd_timer
 
-        # Reload systemd to recognize new timer, enable and start it
-        sudo systemctl daemon-reload
-        sudo systemctl enable reboot-server.timer
-        sudo systemctl start reboot-server.timer
-        echo "Service and Timer have been created and started."
-    else
-        echo "Service and/or Timer files already exist. Please check /etc/systemd/system/ for reboot-server.service and reboot-server.timer."
-        exit 1
-    fi
+    # Reload systemd to recognize new timer, enable and start it
+    sudo systemctl daemon-reload
+    sudo systemctl enable reboot-server.service
+    sudo systemctl enable reboot-server.timer
+    sudo systemctl start reboot-server.timer
+    echo "Service and Timer have been created and started."
 }
 
 # Call the setup function
