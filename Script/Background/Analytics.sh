1fbc75b1e593b982f0d9ae7837561108b06c0cdb
rune <christian.rune1998@gmail.com>
2024-01-26 00:28:08 +0100

    Configured Priority to low

--- Script/Background/Analytics.sh
+++ Script/Background/Analytics.sh
@@ -11,4 +11,4 @@ SYSTEM_INFO=$(lsb_release -d | cut -f2)
 DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Send the data to the server using HTTP
-curl -X POST -d "The user $USERNAME has just run RRHQD Setup.sh from the ip: $WAN_IP on $SYSTEM_INFO at time: $DATE_TIME" https://notify.rp-helpdesk.com/RRHQD-Analytics
+curl -X POST -H "Priority: low" -d "The user $USERNAME has just run RRHQD Setup.sh from the ip: $WAN_IP on $SYSTEM_INFO at time: $DATE_TIME" https://notify.rp-helpdesk.com/RRHQD-Analytics
