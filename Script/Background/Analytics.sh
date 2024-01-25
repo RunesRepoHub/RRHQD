9f3d0c849a2dc09ff9c335aeb27c42b0a4923591
rune <christian.rune1998@gmail.com>
2024-01-26 00:40:59 +0100

    Testing

--- Script/Background/Analytics.sh
+++ Script/Background/Analytics.sh
@@ -1,8 +1,18 @@
-# Get the WAN IP address using dig and OpenDNS resolver
-WAN_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+#!/bin/bash
 
-# Get the username
-USERNAME=$(whoami)
+# Use dialog to prompt the user for their GitHub username
+USERNAME=$(dialog --title "Username" --inputbox "Enter your username:" 8 50 3>&1 1>&2 2>&3)
+
+# Check if the USERNAME variable is empty
+if [ -z "$USERNAME" ]; then
+    echo "No GitHub username entered. Exiting script."
+    exit 1
+fi
+
+# Exit if the user canceled the dialog
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 # Get system distribution and version information
 SYSTEM_INFO=$(lsb_release -d | cut -f2)
@@ -11,4 +21,4 @@ SYSTEM_INFO=$(lsb_release -d | cut -f2)
 DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Send the data to the server using HTTP
-curl -X POST -H "Priority: low" -d "The user $USERNAME has just run RRHQD Setup.sh from the ip: $WAN_IP on $SYSTEM_INFO at time: $DATE_TIME" https://notify.rp-helpdesk.com/RRHQD-Analytics
+curl -X POST -H "Priority: low" -d "The user $USERNAME has just run RRHQD Setup.sh on $SYSTEM_INFO at time: $DATE_TIME" https://notify.rp-helpdesk.com/RRHQD-Analytics
