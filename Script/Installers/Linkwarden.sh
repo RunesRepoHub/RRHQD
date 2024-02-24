58ef785a6ec3f51edc85e6acc1c3bc70cdb14cc8
rune <christian.rune1998@gmail.com>
2024-02-24 21:39:40 +0100

    Hotfix guide menu size

--- Script/Installers/Linkwarden.sh
+++ Script/Installers/Linkwarden.sh
@@ -47,12 +47,12 @@ cd linkwarden
 
 dialog --title "Learn more?" --yesno "Do you want to learn more about how to set it up?" 7 60
 if [ $? -eq 0 ]; then
-    dialog --title "Set up instructions" --msgbox "Follow the setup instructions to configure the system." 5 60
-    dialog --title "Nextauth secret" --msgbox "NEXTAUTH_SECRET should look like '^7yTjn@G$j@KtLh9&@UdMpdfDZ'" 5 60
+    dialog --title "Set up instructions" --msgbox "Follow the setup instructions to configure the system." 15 60
+    dialog --title "Nextauth secret" --msgbox "NEXTAUTH_SECRET should look like '^7yTjn@G$j@KtLh9&@UdMpdfDZ'" 15 60
     dialog --title "Nextauth URL" --msgbox "NEXTAUTH_URL should look like 'http://localhost:3000/api/v1/auth' this can also be a FQDN or IP if FQDN then https:// and no ports" 5 60
-    dialog --title "Postgres password" --msgbox "POSTGRES_PASSWORD should be set to a strong password" 5 60
+    dialog --title "Postgres password" --msgbox "POSTGRES_PASSWORD should be set to a strong password" 15 60
 else
-    dialog --title "Skipping setup instructions" --msgbox "Skipping setup instructions." 5 60
+    dialog --title "Skipping setup instructions" --msgbox "Skipping setup instructions." 15 60
 fi
 
 # Configure Environment Variables interactively using dialog
