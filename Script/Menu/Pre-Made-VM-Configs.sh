11be0f15f35c7a6bde7aa3e2bdcfff1eb8014434
rune <christian.rune1998@gmail.com>
2024-02-29 15:01:10 +0100

    Hotfix

--- Script/Menu/Pre-Made-VM-Configs.sh
+++ Script/Menu/Pre-Made-VM-Configs.sh
@@ -6,20 +6,33 @@ clear
 
 script_name=$(basename "$0" .sh)
 
-dialog --clear
-dialog --backtitle "RRHQD (RunesRepoHub Quick Deploy) - Pre-Made-VM-Configs" \
-       --title "Main Menu - $script_name" \
-       --menu "Please select an option:" 15 60 8 \
-       1 "Run the RP-Helpdesk Standard Config" \
-       2 "Back To Main Menu" 2>"${INPUT}"
+function show_dialog_menu() {
+    dialog --clear
+    dialog --backtitle "RRHQD (RunesRepoHub Quick Deploy) - Pre-Made-VM-Configs" \
+           --title "Main Menu - $script_name" \
+           --menu "Please select an option:" 15 60 8 \
+           1 "Run the RP-Helpdesk Standard Config" \
+           2 "Back To Main Menu" 2>"${INPUT}"
 
-menu_choice=$(<"${INPUT}")
+    menu_choice=$(<"${INPUT}")
 
-case $menu_choice in
-    1)
-        bash $ROOT_FOLDER/$SCRIPT_FOLDER/$PRE_MADE_VM_CONFIGS_DIR/$PRE_MADE_VM
-        ;;
-    *)
-        exit 0
-        ;;
-esac
+    case $menu_choice in
+        1)
+            bash $ROOT_FOLDER/$SCRIPT_FOLDER/$PRE_MADE_VM_CONFIGS_DIR/$PRE_MADE_VM
+            ;;
+        *)
+            exit 0
+            ;;
+    esac
+}
+
+# Define the input file for dialog selections
+INPUT=/tmp/menu.sh.$$
+
+# Ensure the temp file is removed upon script termination
+trap "rm -f $INPUT" 0 1 2 5 15
+
+# Main loop
+while true; do
+    show_dialog_menu
+done
