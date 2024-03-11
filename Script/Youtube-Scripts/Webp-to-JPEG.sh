5eecff3ab6418cf5e98c1576244f76dc4ca59f70
rune <christian.rune1998@gmail.com>
2024-03-11 21:53:27 +0100

    Testing progress bar

--- Script/Youtube-Scripts/Webp-to-JPEG.sh
+++ Script/Youtube-Scripts/Webp-to-JPEG.sh
@@ -3,25 +3,10 @@
 source ~/RRHQD/Core/Core.sh
 
 
+# count amount of webp files to convert
+NUM_FILES=$(find ~/plex/media/youtube -type f -name '*.webp' | wc -l)
 
-# get list of subfolders in ~/plex/media/youtube
-subfolders=(~/plex/media/youtube/*)
-
-# create array with folder names only
-folders=()
-for folder in "${subfolders[@]}"; do
-    folders+=("${folder##*/}")
-done
-
-# show dialog to select one of the folders
-selected_folder=$(dialog --title "Select Folder" --menu "Choose a folder" 20 75 15 "${folders[@]}" 2>&1 >/dev/tty)
-
-# exit if nothing was selected
-if [ -z "$selected_folder" ]; then
-    exit 0
-fi
-
-
+printf "Converting %s WebP images to JPEG..." "$NUM_FILES"
 
 # install cwebp if not already installed
 if ! command -v cwebp &> /dev/null; then
@@ -30,17 +15,20 @@ fi
 
 
 # find all webp files in all subfolders
-find "$selected_folder" -type f -name '*.webp' -print0 |
+find ~/plex/media/youtube -type f -name '*.webp' -print0 |
+    pv -lt > /dev/null |
     while IFS= read -r -d '' file; do
         # get filename without extension
         filename=$(basename "$file" .webp)
         # convert webp to jpeg
-        cwebp -quiet "$file" -o "${file%.*}.jpeg"
-    done
+        cwebp -quiet "$file" -o "${file%.*}.jpeg" &
+        NUM_FILES=$((NUM_FILES - 1))
+    done |
+    pv -N "Converting to JPEG: " -l $((NUM_FILES)) > /dev/null
 
 
 # check if the conversion was successful or if it failed
-if [ "$(find "$selected_folder" -type f -name '*.jpeg' | wc -l)" -lt "$(find "$selected_folder" -type f -name '*.webp' | wc -l)" ]; then
+if [ "$(find ~/plex/media/youtube -type f -name '*.jpeg' | wc -l)" -lt "$(find ~/plex/media/youtube -type f -name '*.webp' | wc -l)" ]; then
     dialog --title "Error" --msgbox "Failed to convert all WebP images to JPEG. Please check the logs for details." 6 60
 else
     dialog --title "Success" --msgbox "All WebP images have been converted to JPEG." 6 60
