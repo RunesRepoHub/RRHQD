1b65dabc05738f0a1d16cc3256eaee56e186324c
rune <christian.rune1998@gmail.com>
2024-03-11 21:45:58 +0100

    Updated webp converter to a user folder option

--- Script/Youtube-Scripts/Webp-to-JPEG.sh
+++ Script/Youtube-Scripts/Webp-to-JPEG.sh
@@ -3,6 +3,26 @@
 source ~/RRHQD/Core/Core.sh
 
 
+
+# get list of subfolders in ~/plex/media/youtube
+subfolders=(~/plex/media/youtube/*)
+
+# create array with folder names only
+folders=()
+for folder in "${subfolders[@]}"; do
+    folders+=("${folder##*/}")
+done
+
+# show dialog to select one of the folders
+selected_folder=$(dialog --title "Select Folder" --menu "Choose a folder" 20 75 15 "${folders[@]}" 2>&1 >/dev/tty)
+
+# exit if nothing was selected
+if [ -z "$selected_folder" ]; then
+    exit 0
+fi
+
+
+
 # install cwebp if not already installed
 if ! command -v cwebp &> /dev/null; then
     sudo apt-get install -y webp
@@ -10,7 +30,7 @@ fi
 
 
 # find all webp files in all subfolders
-find ~/plex/media/youtube -type f -name '*.webp' -print0 |
+find "$selected_folder" -type f -name '*.webp' -print0 |
     while IFS= read -r -d '' file; do
         # get filename without extension
         filename=$(basename "$file" .webp)
@@ -20,7 +40,7 @@ find ~/plex/media/youtube -type f -name '*.webp' -print0 |
 
 
 # check if the conversion was successful or if it failed
-if [ "$(find ~/plex/media/youtube -type f -name '*.jpeg' | wc -l)" -lt "$(find ~/plex/media/youtube -type f -name '*.webp' | wc -l)" ]; then
+if [ "$(find "$selected_folder" -type f -name '*.jpeg' | wc -l)" -lt "$(find "$selected_folder" -type f -name '*.webp' | wc -l)" ]; then
     dialog --title "Error" --msgbox "Failed to convert all WebP images to JPEG. Please check the logs for details." 6 60
 else
     dialog --title "Success" --msgbox "All WebP images have been converted to JPEG." 6 60
